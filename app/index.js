469c1868fc4497612dedfb2faddf3714f02f894f
rozzy <rosekaremeri@gmail.com>
2024-04-19 11:17:12 +0300

    Set Up: install packages and set up projects

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>Home Page</Text>
+      <TouchableOpacity>A Coffee</TouchableOpacity>
+    </View>
+  )
+}
+
+export default index
