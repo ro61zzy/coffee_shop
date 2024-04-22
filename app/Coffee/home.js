5b3de5354aa1018ea740d7c464ade8f66721ab1a
rozzy <rosekaremeri@gmail.com>
2024-04-22 09:46:39 +0300

    Fix: page two

--- app/Coffee/home.js
+++ app/Coffee/home.js
@@ -1,10 +1,36 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  Image,
+} from "react-native";
 import { Stack, useRouter } from "expo-router";
-import coffeeImage from '../../assets/coffee.svg'
+import { Picker } from "@react-native-picker/picker";
+import coffeeImage from "../../assets/coffee.svg";
+import User from "../../assets/user.jpg";
 
 const home = () => {
   const router = useRouter();
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching countries:", error);
+      });
+  }, []);
+
+  const handleCountrySelect = (itemValue) => {
+    setSelectedCountry(itemValue);
+  };
 
   const handleDetail = () => {
     router.push("coffee/detail");
@@ -18,12 +44,61 @@ const home = () => {
           //   headerLeft: () => <DrawerToggleButton />,
         }}
       />
-      <View style={styles.container}>
-      <Text style={styles.text}>All coffees</Text>
-      <TouchableOpacity onPress={handleDetail}>
-        <Text style={styles.button}>Choose A coffee</Text>
-      </TouchableOpacity>
-    </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width:"86%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={
+            {
+                flexDirection: "column",
+                width: "50%",
+              // backgroundColor:"red"
+            }
+          }
+        >
+          <Text>Location</Text>
+          <Picker
+            selectedValue={selectedCountry}
+            onValueChange={(itemValue) => handleCountrySelect(itemValue)}
+            style={styles.picker}
+          >
+            <Picker.Item label="countries" value={null} />
+            {countries.map((country) => (
+              <Picker.Item
+                key={country.cca3}
+                label={country.name.common}
+                value={country.name.common}
+              />
+            ))}
+          </Picker>
+          {/* {selectedCountry && (
+            <Text style={styles.selectedCountry}>
+              Selected country: {selectedCountry}
+            </Text>
+          )}*/}
+        </View>
+        <View>
+          <Image
+            source={User}
+            style={{
+              height: 40,
+              width: 40,
+              borderRadius: 3,
+            }}
+          />
+        </View>
+      </View>
+      {/* <View style={{}}>
+        <TouchableOpacity onPress={handleDetail}>
+          <Text style={styles.button}>Choose A coffee</Text>
+        </TouchableOpacity>
+      </View> */}
     </View>
   );
 };
@@ -31,23 +106,28 @@ const home = () => {
 export default home;
 
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        // alignItems: 'center',
-        // justifyContent: 'center',
-        height: 800
-      },
-      text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        color: 'white',
-      },
-      button: {
-        fontSize: 18,
-        color: 'white',
-        backgroundColor: 'black',
-        padding: 10,
-        borderRadius: 5,
-      },
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  picker: {
+    width: "100%",
+    height: 50,
+    marginBottom: 20,
+    backgroundColor: "#fafafa",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  selectedCountry: {
+    fontSize: 18,
+    marginTop: 20,
+  },
+});
